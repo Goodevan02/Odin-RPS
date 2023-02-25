3667d4f43812d62087ca9b3057b46c2f521f2a16
Evan Good <goodevan02@gmail.com>
2023-02-24 21:50:30 -0600

    remove console.logs fix syntax errors add testing function

--- rps.js
+++ rps.js
@@ -1,58 +1,58 @@
 //RPS Value variables
-const rock = "rock"
-const paper = "paper"
-const scissors = "scissors"
-const playerWins = "The Player Wins"
-const cpuWins = "The CPU Wins"
-con
+const rock = "rock" ;
+const paper = "paper";
+const scissors = "scissors";
+const playerWins = "The Player Wins";
+const cpuWins = "The CPU Wins";
 //generate and return a random RPS choice
 function getComputerChoice() {
     //Define array with each choice in rps
     const rps = [rock, paper, scissors];
     // choose a random play from rps array
-    let randomChoice = rps[Math.floor(Math.random() * rps.length)]
-    console.log (randomChoice)
-    return randomChoice
+    let randomChoice = rps[Math.floor(Math.random() * rps.length)];
+    return randomChoice;
 }
 // Throw and return the players choice
 function getPlayerChoice(){
-    let playerChoice = window.prompt("rps").toLowerCase
-    return playerChoice
+    let playerChoice = window.prompt("rps", "").toLowerCase();
+    return playerChoice;
 }
 //logic to decide who won the round based off the player and cpu RPS choices
 function playRound(playerChoice, cpuChoice) {
-    let winner
     //if values match get new values and restart round
     if (playerChoice === cpuChoice) {
-        console.log(`you both chose ${playerChoice} go again till someone wins the round`)
-       
+        console.log(`you both chose ${playerChoice} go again till someone wins the round`);
+        return "tie"
     // decide winner of rps if player chooses rock
     } else if(playerChoice === rock){
         if (cpuChoice === scissors){
-            console.log(playerWins)
-            return playerWins
+            return playerWins;
         } if (cpuChoice === paper){
-            console.log(cpuWins)
-            return cpuWins
+            return cpuWins;
         }
     }  
+    //decide winner if player chooses paper
     else if(playerChoice === paper){
         if (cpuChoice === rock){
-            console.log(playerWins)
-            return playerWins
+            return playerWins;
         } if (cpuChoice === scissors){
-            console.log(cpuWins)
-            return cpuWins
+            return cpuWins;
         }
     } 
+    //decide winner if player chooses scissors
     else if(playerChoice === scissors){
         if (cpuChoice === paper){
-            console.log(playerWins)
             return playerWins
         } if (cpuChoice === rock){
-            console.log(cpuWins)
             return cpuWins
         }
     } 
 }
 
+// Test function to test the rps logic
+function TEST() {
+    const cpuChoice = getComputerChoice();
+    const playerChoice = getPlayerChoice();
+    console.log(`player choice: ${playerChoice} cpu choice ${cpuChoice}`)
+    playRound(playerChoice, cpuChoice);
+}
