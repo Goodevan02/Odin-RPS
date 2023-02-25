8de568925b2016c62328a7199625ffac64a99727
Evan Good <goodevan02@gmail.com>
2023-02-24 20:45:25 -0600

    add functions to generate and return both cpu and player values

--- rps.js
+++ rps.js
@@ -1,9 +1,31 @@
+//RPS Value variables
+const rock = "rock"
+const paper = "paper"
+const scissors = "scissors"
+
+//generate and return a random RPS choice
 function getComputerChoice() {
     //Define array with each choice in rps
-    const rps = ["rock", "paper", "scissors"];
+    const rps = [rock, paper, scissors];
     // choose a random play from rps array
     let randomChoice = rps[Math.floor(Math.random() * rps.length)]
     console.log (randomChoice)
+    return randomChoice
+}
+// Throw and return the players choice
+function getPlayerChoice(){
+    let playerChoice = window.prompt("rps").toLowerCase
+    return playerChoice
+}
+//logic to decide who won the round based off the player and cpu RPS choices
+function playRound(playerChoice, cpuChoice) {
+    let winner
+    if (playerChoice === cpuChoice) {
+        console.log(`you both chose ${playerChoice} go again till someone wins the round`)
+        
+    } else if(playerChoice === rock){
+        if (cpuChoice === )
+
+    }
 }
 
-getComputerChoice()
