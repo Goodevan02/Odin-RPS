51cc5267f3b8814b4a2163b27efeaa165db8388f
Evan Good <goodevan02@gmail.com>
2023-02-24 21:02:37 -0600

    initial rps logic

--- rps.js
+++ rps.js
@@ -2,7 +2,9 @@
 const rock = "rock"
 const paper = "paper"
 const scissors = "scissors"
-
+const playerWins = "The Player Wins"
+const cpuWins = "The CPU Wins"
+con
 //generate and return a random RPS choice
 function getComputerChoice() {
     //Define array with each choice in rps
@@ -20,12 +22,37 @@ function getPlayerChoice(){
 //logic to decide who won the round based off the player and cpu RPS choices
 function playRound(playerChoice, cpuChoice) {
     let winner
+    //if values match get new values and restart round
     if (playerChoice === cpuChoice) {
         console.log(`you both chose ${playerChoice} go again till someone wins the round`)
-        
+       
+    // decide winner of rps if player chooses rock
     } else if(playerChoice === rock){
-        if (cpuChoice === )
-
-    }
+        if (cpuChoice === scissors){
+            console.log(playerWins)
+            return playerWins
+        } if (cpuChoice === paper){
+            console.log(cpuWins)
+            return cpuWins
+        }
+    }  
+    else if(playerChoice === paper){
+        if (cpuChoice === rock){
+            console.log(playerWins)
+            return playerWins
+        } if (cpuChoice === scissors){
+            console.log(cpuWins)
+            return cpuWins
+        }
+    } 
+    else if(playerChoice === scissors){
+        if (cpuChoice === paper){
+            console.log(playerWins)
+            return playerWins
+        } if (cpuChoice === rock){
+            console.log(cpuWins)
+            return cpuWins
+        }
+    } 
 }
 
